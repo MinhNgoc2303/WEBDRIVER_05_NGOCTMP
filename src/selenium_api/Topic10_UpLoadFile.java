12ea64118d3a496eb109cec810e9c3d865dbd580
Ngoc Tran Minh Phan <ngoctmphan@kms.com.vn>
2018-09-20 17:55:15 +0700

    Update for topic11

--- src/selenium_api/Topic10_UpLoadFile.java
+++ src/selenium_api/Topic10_UpLoadFile.java
@@ -10,11 +10,13 @@ import java.awt.Toolkit;
 import java.awt.datatransfer.StringSelection;
 import java.awt.event.KeyEvent;
 import java.io.IOException;
+import java.util.List;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.Assert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -27,16 +29,26 @@ public class Topic10_UpLoadFile {
 	WebDriver driver;
 
 	String projectDirectory = System.getProperty("user.dir");
+	
+	//File name
 	String fileName = "01.PNG";
-	String uploadFilePath = projectDirectory + "\\image\\" + fileName;
+	String fileName01 = "02.PNG";
+	String fileName02 = "03.PNG";
 	
 	String chromeUpload = projectDirectory +"\\upload\\chrome.exe" ;
 	String firefoxUpload = projectDirectory +"\\upload\\firefox.exe" ;
-	String ieUpload = projectDirectory +"\\upload\\ie.exe" ;
+	String fieUpload = projectDirectory +"\\upload\\ie.exe" ;
 	
 	String folderName = "automation" + randomNumber();
 	String EmailAddress ="ngoc" + randomNumber() + "@gmail.com";
+	
 	String firstName = "NgocPhan";
+	
+	// Image path
+	String uploadFilePath = projectDirectory + "\\image\\" + fileName;
+	String uploadFilePath01 = projectDirectory + "\\image\\" + fileName01;	
+	String uploadFilePath02 = projectDirectory + "\\image\\" + fileName02;
+	
 	@BeforeClass
 	public void beforeClass() {
 		//System.setProperty("webdriver.chrome.driver", ".\\driver\\chromedriver.exe");
@@ -174,8 +186,55 @@ public class Topic10_UpLoadFile {
 		//Step 10 - Verify file name exist in folder (UploadFile.jpg)
 		Assert.assertTrue(driver.findElement(By.xpath("//a[text()='" + fileName + "']")).isDisplayed());
 		
-		
 	}
+	
+	@Test
+	public void TS04_UpLoadMultiple() throws Exception {
+			
+			String file[] = {uploadFilePath, uploadFilePath01, uploadFilePath02 };
+			
+			//Duyet qua tung phan tu cua mang
+			//Mang 3 phan tu bat dau bang vi tri: 0 1 2 
+			for (int i=0;i< file.length; i++) {
+				WebElement uploadElement = driver.findElement(By.xpath("//input[@type='file']"));
+				System.out.println("Item= " + file[i]);
+				uploadElement.sendKeys(file[i]);
+			}
+			driver.get("http://blueimp.github.com/jQuery-File-Upload/");
+
+			
+			String summaryFile = "\"" + uploadFilePath +"\" \"" + uploadFilePath01 + "\" \"" + uploadFilePath02 + "\"";
+			System.out.println(summaryFile);
+			
+			
+			Thread.sleep(4000);
+			
+			//check 3 images displayed
+			Assert.assertTrue(driver.findElement(By.xpath("//p[@class='name' and text()='" + fileName + "']")).isDisplayed());
+			Assert.assertTrue(driver.findElement(By.xpath("//p[@class='name' and text()='" + fileName01 + "']")).isDisplayed());
+			Assert.assertTrue(driver.findElement(By.xpath("//p[@class='name' and text()='" + fileName02 + "']")).isDisplayed());
+			
+			//Click 3 start button
+			List <WebElement> startButton = driver.findElements(By.xpath("//table//button[@class='btn btn-primary start']"));
+			
+			//for-each
+			for (WebElement start : startButton ) {
+				start.click();
+			}
+			
+			//Check 3 images uploaded success
+			Assert.assertTrue(driver.findElement(By.xpath("//p[@class='name']//a[text()='" + fileName + "']")).isDisplayed());
+			Assert.assertTrue(driver.findElement(By.xpath("//p[@class='name']//a[text()='" + fileName01 + "']")).isDisplayed());
+			Assert.assertTrue(driver.findElement(By.xpath("//p[@class='name']//a[text()='" + fileName02 + "']")).isDisplayed());
+			
+			//Check 3 images  real image 
+			List <WebElement> images = driver.findElements(By.xpath("//table[@class='table table-striped']//img"));
+			for (WebElement image :  images) {
+				Assert.assertTrue(checkAnyImageLoaded(image));
+			}
+
+			
+		}
 	
 
 	@AfterClass
@@ -187,6 +246,16 @@ public class Topic10_UpLoadFile {
 		Random random = new Random();
 		return random.nextInt(999999);
 		
+	}
+	
+	 public Object executeJSForWebElement(WebElement element) {
+	          JavascriptExecutor js = (JavascriptExecutor) driver;
+	          return js.executeScript("arguments[0].click();", element);
+	   }
+	
+	public boolean checkAnyImageLoaded (WebElement image) {
+        JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
+        return (boolean)jsExecutor.executeScript("return arguments[0].complete &&()" + "typeof argument[0].naturalWidth !='undefined' && arguments[0].naturalWidth >0",image);
 	}
 
 }
